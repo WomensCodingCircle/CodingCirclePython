7feb74b571067f24c381fb16443919b8fb015747
Charlotte Weaver <weaverc10@janelia.hhmi.org>
2016-02-01 11:24:25 -0500

    Solution to lesson 4

---
+++ Lesson04_Iteration/wandererSOLUTION.py
@@ -0,0 +1,21 @@
+
+wanderer_x = 0
+wanderer_y = 0
+treasure_x = 5
+treasure_y = 1
+while True:
+	direction = raw_input("Which direction would you like to travel in? (N, S, E, or W. done to quit)")
+	if direction == 'E':
+		wanderer_x += 1
+	elif direction == 'W':
+		wanderer_x -= 1
+	elif direction == 'N':
+		wanderer_y += 1
+	elif direction == 'S':
+		wanderer_y -= 1
+	elif direction == 'done':
+		break
+	if (wanderer_y == treasure_y and wanderer_x == treasure_x):
+		print "Congrats you found a gold necklace!"
+		break
+print "You walked to x: " + str(wanderer_x) + " y: " + str(wanderer_y)
