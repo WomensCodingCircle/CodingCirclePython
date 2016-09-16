bf867e4cf61fc0ec7f5bd3aad60797b4c977b0bb
Charlotte Weaver <charlottewsweaver@gmail.com>
2016-09-15 22:47:59 -0400

    Added an easier project: Password Hacker

---
+++ Lesson09_Project/PasswordHacker/password_check.py
@@ -0,0 +1,21 @@
+def check_user_pass(user, password):
+    """
+    Checks to see if the user and password are a match
+    Returns: True if match, False if not
+    """
+	user_pass = {
+		'avery': 'rosebud',
+		'bruce': 'harley',
+		'carla': 'flower',
+		'dennis': 'porsche1',
+		'eve': 'thunder!',
+		'felicity': 'jaguar#1',
+		'greta': 'c00l'
+	}
+	if user_pass[user] == password:
+		return True
+	else:
+		return False
+
+user_list = ['avery', 'bruce', 'carla', 'dennis', 'eve', 'felicity', 'greta']
+
