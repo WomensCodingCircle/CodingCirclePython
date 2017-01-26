f6ca5cbc0834af2f970ad658e2177286366aa883
Charlotte Weaver <weaverc10@janelia.hhmi.org>
2017-01-26 11:34:42 -0500

    python 3 compliant

--- Lesson01_Variables/MilestonesSOLUTION.py
+++ Lesson01_Variables/MilestonesSOLUTION.py
@@ -1,4 +1,4 @@
-birth_year = raw_input("What is the birth year for the milestones?")
+birth_year = input("What is the birth year for the milestones?")
 # raw_input always gives strings, so convert to int
 birth_year = int(birth_year)
 
@@ -7,6 +7,6 @@ driving_year = birth_year + 16
 drinking_year = birth_year + 21
 president_year = birth_year + 35
 
-print "You are able to drive in " + str(driving_year)
-print "You are able to drink in " + str(drinking_year)
-print "You are able to run for president in " + str(president_year)
+print("You are able to drive in " + str(driving_year))
+print("You are able to drink in " + str(drinking_year))
+print("You are able to run for president in " + str(president_year))
